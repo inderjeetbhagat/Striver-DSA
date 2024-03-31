041017b2a820331d38b2812e1d94eac9950187d4
inderjeetbhagat <62701663+inderjeetbhagat@users.noreply.github.com>
2024-03-31 09:08:42 +0530

    Started learning DSA

---
+++ Patterns/P1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void pattern1() {
+    for(int i = 0; i < 4; i++) {
+        
+        for( int j =0; j< 4; j++) {
+            cout<<"* ";
+        }
+        cout<<"\n";
+    }
+}
+
+void pattern2() {
+    
+}
+int main() {
+    pattern1();
+
+}
